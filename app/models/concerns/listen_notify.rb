23737b4053f518e7cf5f1352a78919f5c3f27931
laurpaik <laurpaik@gmail.com>
2017-08-13 09:43:58 -0400

    Annotate Antony's code

--- app/models/concerns/listen_notify.rb
+++ app/models/concerns/listen_notify.rb
@@ -9,7 +9,7 @@ module ListenNotify
   private
 
   def on_update_listen_notify_channel
-    "#{self.class}#update(#{id})"
+    "#{self.class}#update(#{id})" # this is the channel! this is the string we need??
   end
 
   def on_create_listen_notify_channel
@@ -17,8 +17,13 @@ module ListenNotify
   end
 
   def notify_update
+    # e.g. class Channel --> root = "channel"
     root = self.class.to_s.downcase
+    # save the changes in a hash with the root as the key
+    # I hate syntactic sugar
     payload = { root => changes }.to_json
+    # tell postgres to run `notify`
+    # sends a notification event to that channel defined earlier with the payload message
     self.class.connection.execute(
       "NOTIFY \"#{on_update_listen_notify_channel}\", '#{payload}'"
     )
@@ -28,30 +33,39 @@ module ListenNotify
 
   def listen_for_update(timeout)
     connection = self.class.connection
+    # register the current connection/session as a listener
+    # notify_update won't work without this step
     connection.execute "LISTEN \"#{on_update_listen_notify_channel}\""
     timed_out = false
     until timed_out
       timed_out = !connection.raw_connection.wait_for_notify(timeout) do |event, pid, data|
+        # until the connection times out, call the event block? do whatever action we need with the event, pid, or data?
+        # in the controller case, queue.push data
         yield event, data, pid
       end
     end
   ensure
+    # kind of like `finally` --> ensure that eventually we unlisten
     connection.execute "UNLISTEN \"#{on_update_listen_notify_channel}\""
   end
 
   #
   module ClassMethods
     def notify_on_update
+      # after each update, run notify update --> this is what we call in our model
       after_update :notify_update
     end
 
     def notify_create
       connection.execute(
+      # notify the channel with the changes as the payload message
         "NOTIFY \"#{on_create_listen_notify_channel}\", '#{changes}'"
       )
     end
 
     def listen_for_create
+      # is it just comments we're listening for? or do we need to define some other thing?
+      # LISTEN \"#{something here? on_create_listen_notify_channel maybe?}\"
       connection.execute "LISTEN comments"
       loop do
         connection.raw_connection.wait_for_notify do |event, pid, data|
