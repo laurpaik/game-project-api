ed3405c46461c7c1d138bec90888bfed1bea6bbf
laurpaik <laurpaik@gmail.com>
2017-09-13 14:14:41 -0400

    Change `watch` script to customize ID
    - Not just watch for ID=1

--- scripts/channels/watch.sh
+++ scripts/channels/watch.sh
@@ -1,3 +1,3 @@
-curl --include --request GET http://localhost:4741/channels/1/watch \
+curl --include --request GET http://localhost:4741/channels/${ID}/watch \
   --header "Authorization: Token token=$TOKEN" \
   --header "Content-Type: application/json"
